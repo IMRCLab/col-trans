b26ef6c92f2a549ed487dcd690865f8acf2a2ac8
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2022-08-29 17:53:24 +0200

    OSQP example for two UAVs

--- osqp/osqp_example.py
+++ osqp/osqp_example.py
@@ -2,12 +2,41 @@ import osqp
 import numpy as np
 from scipy import sparse
 
-# Define problem data
-P = sparse.csc_matrix([[4, 1], [1, 2]])
-q = np.array([1, 1])
-A = sparse.csc_matrix([[1, 1], [1, 0], [0, 1]])
-l = np.array([1, 0, 0])
-u = np.array([1, 0.7, 0.7])
+"""
+Structure of the QP in OSQP regarding this problem is as follows: 
+min (1/2)mu_des'@P@mu_des + q'@mu_des
+such that : [Fd                     [Aeq,          [Fd
+         np.inf*np.ones(2,)]   <=   Aineq]    <=    np.zeros(2,)]
+
+Where, 
+    Aeq = Allocation Matrix (P in the paper)
+    Aineq: it was computed numerically in pyCrazyflie code having  20 degrees as limit
+            but it can be updated with prob.update(Ax=Ax_new, Ax_idx=Ax_new_idx)
+    Fd: is the desired force applied on the payload.
+        initially it is set to the wieght of the payload, but it can also be updated with
+        prob.update(q=q_new, l=l_new, u=u_new)
+"""
+
+# Aineq: Aineq@mu <= np.zeros(2,) --> Hand crafted plane constraints 
+Aineq = sparse.csc_matrix([[ 0.01938242, -0.10992316, 0.04000876, 0, 0, 0],
+                           [ 0, 0, 0, -0.01938242, 0.10992316, 0.04000876]])
+
+allocMatrix = np.zeros((3, 6)) # AllocMatrix@mu = Fd
+for i in range(0,6,3):
+        allocMatrix[0:3,i:i+3] = np.eye(3)
+
+Aeq   = allocMatrix
+
+# A matrix
+A = sparse.vstack((Aeq, Aineq), format='csc') 
+# P matrix
+P = sparse.csc_matrix(np.eye(6))
+# q vector
+q = np.zeros(6)
+# lower bound--> [Fd, -np.inf*ones(2,)]
+# set Fd to [0,0,0.0981] -->[0,0,mass*gravity]: mass of payload = 0.01 kg, gravity: 9.81 m/s^2
+l = np.hstack(([0,0,0.0981], -np.inf*np.ones(2,)))
+u = np.hstack(([0,0,0.0981], np.zeros(2,)))
 
 # Create an OSQP object
 prob = osqp.OSQP()
