12a6d1b487119e002828bf5b49dfcb4b5b277915
Wolfgang Hoenig <hoenig@tu-berlin.de>
2023-01-06 15:24:26 +0100

    add osqp postprocessing and hyperplane script

---
+++ osqp/hyperplane.py
@@ -0,0 +1,66 @@
+import osqp
+import cvxpy as cp
+import numpy as np
+import scipy.sparse as sp
+
+"""
+
+"""
+
+def main():
+
+        # cvxpy Problem
+        p1 = np.array([1,2,3])
+        p2 = np.array([4,5,6])
+
+        n = cp.Variable(3)
+        prob = cp.Problem(cp.Minimize(cp.sum_squares(n)),
+                    [
+                        n.T @ p1 <= -1,
+                        n.T @ p2 >= 1,
+                    ])
+        prob.solve()
+        print(n.value)
+
+        # extract matrices
+        data, chain, inverse_data = prob.get_problem_data(cp.OSQP)
+        print(data)
+
+        # x.T P x + q.T x s.t.
+        # l <= A x <= u
+        P = data['P']
+        print("P", P.todense()) # objective
+        q = data['q']
+        print("q", q) # objective
+
+        # see https://github.com/cvxpy/cvxpy/issues/477 and
+        # https://github.com/cvxpy/cvxpy/blob/ef3c3ed2627e9ff26b62ca9c771e0f3b0c693905/cvxpy/reductions/solvers/qp_solvers/osqp_qpif.py#L56-L63
+        A = sp.vstack([data['A'], data['F']]).tocsc()
+        print("A", A.todense())
+        u = np.concatenate([data['b'], data['G']])
+        print("u", u)
+        l = np.concatenate([data['b'], -np.inf*np.ones(data['G'].shape)])
+        print("l", l)
+
+        # generate the code
+
+        # Create an OSQP object
+        prob = osqp.OSQP()
+
+        # Setup workspace and change alpha parameter
+        prob.setup(P, q, A, l, u, alpha=1.0)
+
+        prob.codegen("cffirmware_osqp/src/generated",
+                project_type='Makefile',
+                parameters='matrices',
+                python_ext_name='emosqp',
+                force_rewrite=True,
+                FLOAT=True,
+                LONG=False)
+
+        # Solve problem
+        res = prob.solve()
+
+
+if __name__ == '__main__':
+    main()
---
+++ osqp/postprocessing.py
@@ -0,0 +1,21 @@
+
+
+def postprocess(name, output, input = "cffirmware_osqp/src/generated/src/osqp/workspace.c"):
+    """ Adds static keyword, so the workspace can be re-used """
+        
+    with open(input) as f:
+        contents = f.read()
+
+    datatypes = ["c_int", "c_float", "csc", "OSQPData", "OSQPSettings", "OSQPScaling", "QDLDL_float", "QDLDL_int", "QDLDL_bool", "qdldl_solver", "OSQPSolution", "OSQPInfo"]
+
+    for datatype in datatypes:
+        contents = contents.replace("{} ".format(datatype), "static {} ".format(datatype))
+
+    contents = contents.replace("OSQPWorkspace workspace", "OSQPWorkspace {}".format(name))
+
+    with open(output, 'w') as f:
+        f.write(contents)
+
+
+if __name__ == '__main__':
+    postprocess("hyperplane", "../crazyflie-firmware/src/lib/osqp/src/osqp/workspace_hyperplane.c")
