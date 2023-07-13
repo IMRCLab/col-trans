5ec1c32e490a0866a7bf2a05ff14276fd621ce97
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2023-07-13 14:31:31 +0200

    generate qp  workspace for n uavs point mass case

--- osqp/gen_qp_code.py
+++ osqp/gen_qp_code.py
@@ -5,6 +5,7 @@ from scipy import sparse
 from scipy.linalg import block_diag
 from postprocessing import postprocess
 from helper import generate_block_diagonal
+import matplotlib.pyplot as plt
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
 
@@ -13,11 +14,15 @@ def genOSQPprob(nhps, n, loadType, qpWorkspace_fileName) -> None:
     
     Aineq = sparse.csc_matrix(generate_block_diagonal(nhps, n))
     if loadType == "point":
-        Aeq = np.ones((3, n*3))
+        Aeq = np.zeros((3, n*3))
+        for i in range(0, Aeq.shape[1],3):
+            Aeq[0:3, i:i+3] = np.eye(3)
         l = np.hstack((np.ones(3,), -np.inf*np.ones(nhps,)))
         u = np.hstack((np.ones(3,), np.zeros(nhps,)))
     elif loadType == "rigid":
         Aeq = np.ones((6, n*3))
+        for i in range(0, Aeq.shape[1],3):
+            Aeq[0:3, i:i+3] = np.eye(3)
         l = np.hstack((np.ones(6,), -np.inf*np.ones(nhps,)))
         u = np.hstack((np.ones(6,), np.zeros(nhps,)))
     else:
@@ -40,20 +45,9 @@ def genOSQPprob(nhps, n, loadType, qpWorkspace_fileName) -> None:
         FLOAT=True,
         LONG=False)
     # "../crazyflie-firmware/src/lib/osqp/src/osqp/"
-    postprocess(qpWorkspace_fileName,  qpWorkspace_fileName +".c")
-
-def createCode(n, loadType):
-    if loadType == "point":
-        
-        pass
-    elif loadType == "rigid":
-        pass
-    else:
-        pass
-
+    postprocess(qpWorkspace_fileName,  "../crazyflie-firmware/src/lib/osqp/src/osqp/"+ qpWorkspace_fileName +".c")
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--uavs', type=int, help="num of uavs")
     parser.add_argument("--mass",default="point", type=str, help="point or rigid. If no input, it will assume a point mass")
@@ -62,17 +56,11 @@ def main():
     n = args.uavs #number of uavs
     nhps = (n)*(n-1) #total number of hps
     loadType = args.mass # load type
-    qpWorkspace_fileName = "workspace_"+str(n)+"uav_" + str(int(nhps/2))
+    qpWorkspace_fileName = "workspace_"+str(n)+"uav_" + str(int(nhps/2 - 1)) + "hp"
 
     # generate the OSQP problem
     genOSQPprob(nhps, n, loadType, qpWorkspace_fileName)
 
-    filename = qpWorkspace_fileName + '_code' + ".c"
-    # generate the text for the code
-    # code = createCode(n, loadType)
-    # # write the code in file
-    # with open(filename, "w") as f:
-    #     file.write(code)
     
 if __name__ == "__main__":
     main()
